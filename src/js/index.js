dbf1ddaa0791e8ed4e28de40f4284e97551c41be
Daniel Jiménez <elvvira.castellanos@gmail.com>
2023-02-22 12:37:41 +0100

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+// import catIMage from '../assets/images/cat.jpeg'
+// const img = document.createElement('img');
+// img.src = catImage;
+// document.body.append(img);
 import { sayHello } from './demo.js';
-
 sayHello();
